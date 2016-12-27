a76b5ff9b84a74d17faa3af604256089f7f05996
David Rehmat <me@dave.blue>
2016-12-27 13:13:55 -0500

    completed command-intent-digest
    will search as the man in the middle for command interp so the executors
    do not have to concern themselves with command validation

--- src/app/model/command/command-intent-digest.ts
+++ src/app/model/command/command-intent-digest.ts
@@ -3,16 +3,33 @@ import {UpText} from "../up-text";
 import {CommandIntent} from "./command-intent";
 export class CommandIntentDigest {
 
-    public runtime: CommandRuntime;
-    public command: UpText;
+    constructor(public runtime: CommandRuntime = CommandRuntime.PLATFORM,
+                public command: UpText = new UpText()) {
+    }
 
+    public isEmpty():boolean {
+        return !!this.command.content.length;
+    }
 }
 export namespace CommandIntentDigest {
 
-    export const from = (intent: CommandIntent):CommandIntentDigest => {
+    export const from = (intent: CommandIntent): CommandIntentDigest => {
         const digest = new CommandIntentDigest();
 
-        digest.runtime = CommandRuntime.USER;
+        let input = intent.command.trim();
+
+        if (input.length) {
+            let runtimeFlag: string = input.charAt(0);
+            let runtime: CommandRuntime = CommandRuntime.from(runtimeFlag);
+            //": reload"\\
+            if (runtime !== null) {
+                digest.runtime = runtime;
+                //"reload"\\
+                input = input.substring(0).trim();
+            }
+
+            digest.command.content = input;
+        }
 
         return digest;
     }
--- src/app/model/command/command-runtime.ts
+++ src/app/model/command/command-runtime.ts
@@ -1,5 +1,35 @@
 export enum CommandRuntime {
     SYSTEM,
     INTERNAL,
-    USER
+    PLATFORM,
+    PLATFORM_REACTIVE
+}
+export namespace CommandRuntime {
+
+    export const FLAG_SYSTEM: string = ">";
+    export const FLAG_INTERNAL: string = ":";
+    export const FLAG_PLATFORM: string = "#";
+    export const FLAG_PLATFORM_REACTIVE: string = "*";
+
+    export const from = (flag: string):CommandRuntime => {
+        if(!flag || !(flag = flag.trim()).length) {
+            return null;
+        }
+
+        if(flag === FLAG_PLATFORM) {
+            return CommandRuntime.PLATFORM;
+        }
+        if(flag === FLAG_SYSTEM) {
+            return CommandRuntime.SYSTEM;
+        }
+        if(flag === FLAG_INTERNAL) {
+            return CommandRuntime.INTERNAL;
+        }
+        if(flag === FLAG_PLATFORM_REACTIVE) {
+            return CommandRuntime.PLATFORM_REACTIVE;
+        }
+
+        return null;
+    }
+
 }
