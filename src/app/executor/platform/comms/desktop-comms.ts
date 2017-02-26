6cf244b52c1d3c009bd7f6658800df0a6593f204
David Rehmat <me@dave.blue>
2017-02-26 16:09:57 -0500

    clipboard control on the desktop module

--- src/app/executor/platform/comms/desktop-comms.ts
+++ src/app/executor/platform/comms/desktop-comms.ts
@@ -1,8 +1,7 @@
 import {PlatformCommsHandler} from "../platform-comms-handler";
 import {Host} from "../../../model/host";
 import * as path from 'path';
-
-const {shell} = require('electron');
+const {shell, clipboard} = require('electron');
 
 export class DesktopComms extends PlatformCommsHandler {
 
@@ -14,6 +13,18 @@ export class DesktopComms extends PlatformCommsHandler {
 
         this.add('beep', DesktopComms.beep);
         this.add('openLocation', DesktopComms.openLocation);
+
+        this.add('setFindText', DesktopComms.setFindText);
+        this.add('setClipboard', DesktopComms.setClipboard);
+
+        this.add('getClipboardFormats', DesktopComms.getClipboardFormats);
+        this.add('getClipboardBookmark', DesktopComms.getClipboardBookmark);
+        this.add('getClipboardRTF', DesktopComms.getClipboardRTF);
+        this.add('getClipboardImage', DesktopComms.getClipboardImage);
+        this.add('getClipboardHTML', DesktopComms.getClipboardHTML);
+        this.add('getClipboardText', DesktopComms.getClipboardText);
+        this.add('getFindText', DesktopComms.getFindText);
+
         this.add('moveItemToTrash', DesktopComms.moveItemToTrash, {
             path: 'The location of the file to delete'
         });
@@ -25,13 +36,105 @@ export class DesktopComms extends PlatformCommsHandler {
         });
     }
 
-    static moveItemToTrash(host:Host,
-                           location:string,
+    static getClipboardBookmark(host:Host, format:string) {
+        if (format && typeof format === 'string') {
+            return clipboard.readBookmark(format);
+        }
+
+        return clipboard.readBookmark();
+    }
+
+    static getClipboardRTF(host:Host, format:string) {
+        if (format && typeof format === 'string') {
+            return clipboard.readRTF(format);
+        }
+
+        return clipboard.readRTF();
+    }
+
+    static getClipboardImage(host:Host, format:string) {
+        if (format && typeof format === 'string') {
+            return clipboard.readImage(format).toDataURL();
+        }
+
+        return clipboard.readImage().toDataURL();
+    }
+
+    static getClipboardHTML(host:Host, format:string) {
+        if (format && typeof format === 'string') {
+            return shell.readHTML(format);
+        }
+
+        return clipboard.readHTML();
+    }
+
+    static getClipboardText(host:Host, format:string) {
+        if (format && typeof format === 'string') {
+            return clipboard.readText(format);
+        }
+
+        return clipboard.readText();
+    }
+
+    static setClipboard(host: Host,
+                        {mappings, type}:any) {
+
+
+        if (mappings
+            && mappings.image
+            && !path.isAbsolute(mappings.image)) {
+
+            mappings.image = path.join(host.cwd(), mappings.image);
+        }
+
+        if(type && typeof type === 'string') {
+            return clipboard.write(mappings, type) || '';
+        }
+
+        return clipboard.write(mappings) || '';
+    }
+
+    static getClipboardFormats(host: Host, format: string) {
+
+        if (format && typeof format === 'string') {
+            return clipboard.availableFormats(format);
+        }
+
+        return clipboard.availableFormats();
+    }
+
+    static getFindText() {
+
+        return clipboard.readFindText();
+    }
+
+    static setFindText(host: Host,
+                       text: string) {
+        text = text || '';
+
+        return clipboard.writeFindText(text) || '';
+    }
+
+    static openURI(host: Host,
+                   location: string,
+                   resolve: (message?: any) => any,
+                   reject: (message?: string, syntax?: boolean) => any) {
+
+        if (!location) {
+            reject('Provide the URI of the target', true);
+            return;
+        }
+
+        return shell.openExternal(location);
+    }
+
+    static moveItemToTrash(host: Host,
+                           location: string,
                            resolve: (message?: any) => any,
                            reject: (message?: string, syntax?: boolean) => any) {
 
-        if(!location) {
-            reject('Provide the path of the target');
+        if (!location) {
+            reject('Provide the path of the target', true);
             return;
         }
         if (!path.isAbsolute(location)) {
@@ -41,22 +144,9 @@ export class DesktopComms extends PlatformCommsHandler {
         return shell.moveItemToTrash(location);
     }
 
-    static openURI(host:Host,
-                location:string,
-                resolve: (message?: any) => any,
-                reject: (message?: string, syntax?: boolean) => any) {
+    static openLocation(host: Host, location: string) {
 
-        if(!location) {
-            reject('Provide the URI of the target');
-            return;
-        }
-
-        return shell.openExternal(location);
-    }
-
-    static openLocation(host:Host, location:string) {
-
-        if(!location) {
+        if (!location) {
             location = host.cwd();
         }
 
@@ -67,13 +157,13 @@ export class DesktopComms extends PlatformCommsHandler {
         return shell.showItemInFolder(location);
     }
 
-    static open(host:Host,
-                location:string,
-                           resolve: (message?: any) => any,
-                           reject: (message?: string, syntax?: boolean) => any) {
+    static open(host: Host,
+                location: string,
+                resolve: (message?: any) => any,
+                reject: (message?: string, syntax?: boolean) => any) {
 
-        if(!location) {
-            reject('Provide the path of the target');
+        if (!location) {
+            reject('Provide the path of the target', true);
             return;
         }
         if (!path.isAbsolute(location)) {
--- src/app/executor/platform/modules/desktop.js
+++ src/app/executor/platform/modules/desktop.js
@@ -2,6 +2,87 @@ let comms = require('comms');
 
 let Desktop = module.exports = {};
 
+Desktop.TEXT = 'text';
+Desktop.HTML = 'html';
+Desktop.IMAGE = 'image';
+Desktop.RTF = 'rtf';
+Desktop.BOOKMARK = 'bookmark';
+
+Desktop.setFindText = function(text) {
+    return comms.message('DESKTOP.setFindText', text);
+};
+
+Desktop.setClipboard = function(result, format) {
+    let message = {};
+
+    if (typeof result === 'string') {
+        message.mappings = {
+            text: result
+        }
+    } else {
+        message.mappings = result;
+    }
+
+    message.format = format;
+
+    return comms.message('DESKTOP.setClipboard', message);
+};
+
+Desktop.setClipboardText = (text, format) => Desktop.setClipboard({
+    text: text
+}, format);
+
+Desktop.setClipboardHTML = (html, format) => Desktop.setClipboard({
+    html: html
+}, format);
+
+Desktop.setClipboardImage = (path, format) => Desktop.setClipboard({
+    image: path
+}, format);
+
+Desktop.setClipboardRTF = (rft, format) => Desktop.setClipboard({
+    rft: rft
+}, format);
+
+Desktop.setClipboardBookmark = (bookmark, format) => Desktop.setClipboard({
+    bookmark: bookmark
+}, format);
+
+Desktop.setFindText = function(text) {
+    return comms.message('DESKTOP.setFindText', text);
+};
+
+Desktop.getClipboardFormats = function(format) {
+    return comms.message('DESKTOP.getClipboardFormats', format);
+};
+
+Desktop.getClipboardBookmark = function(format) {
+    return comms.message('DESKTOP.getClipboardBookmark', format);
+};
+
+Desktop.getClipboardRTF = function(format) {
+    return comms.message('DESKTOP.getClipboardRTF', format);
+};
+
+Desktop.getClipboardImage = function(format) {
+    return comms.message('DESKTOP.getClipboardImage', format);
+};
+
+Desktop.getClipboardHTML = function(format) {
+    return comms.message('DESKTOP.getClipboardHTML', format);
+};
+
+Desktop.getClipboardText = function(format) {
+    return comms.message('DESKTOP.getClipboardText', format);
+};
+
+Desktop.getFindText = function() {
+    return comms.message('DESKTOP.getFindText');
+};
+
+
+
+
 Desktop.beep = function() {
     return comms.message('DESKTOP.BEEP');
 };
